67b48372e45ae9a87da890293fa393a3762dfa9c
Marlonfpereira <marlon.fa.pereira@hotmail.com>
2023-01-21 15:23:59 -0300

    Elaboração do método caminhar()

--- antColony.py
+++ antColony.py
@@ -1,54 +1,54 @@
 import numpy as np
 import pandas as pd
 
-nGrafo = 10 #input("Qual grafo deseja carregar:")
-path = f'./entradasGrafos/Entrada {nGrafo}.txt'
-#data = pd.read_csv(path, index_col=None, header=0, sep=' ', false_values=np.NaN)
-data = pd.read_csv(path, index_col=None, header=None)
-
-
-# teste = np.zeros((nGrafo,nGrafo))
-# print(teste[1])
-
-# teste[1][2] = 7
-
-# print(teste[1])
-
-
 class Rotas:
     def __init__(self, distancia, umSobreD, feromonio):
         self.distancia = distancia
         self.umSobreD = umSobreD
         self.feromonio = feromonio
 
-teste = list()
+nGrafo = 10 #input("Qual grafo deseja carregar:")
+nAnts = 10
+path = f'./entradasGrafos/Entrada {nGrafo}.txt'
+dist = pd.read_csv(path, index_col=None, header=None)
+
+data = list()
 for i in range(nGrafo):
     aux = list()
-    teste.append(aux)
+    data.append(aux)
     for j in range(nGrafo):
-        teste[i].append(0)
-        
-for i in range(nGrafo):
-    for j in range(nGrafo):
-        rota = Rotas(data[i][j], 1/data[i][j] if data[i][j] else 0, 1)
-        teste[i][j] = rota
-        
-print(teste[0][3].distancia)
-print(teste[0][4].distancia)
-
+        rota = Rotas(dist[i][j], 1/dist[i][j] if dist[i][j] else 0, 1)
+        data[i].append(rota)
+del dist
 
 class Formiga:
-    def __init__(self, caminho, distancia):
+    def __init__(self, caminho, distTotal):
         self.caminho = caminho
-        self.distancia = distancia
+        self.distTotal = distTotal
 
-    # def caminhar(self):
-    #     print("Caminhei, foda")
+    def caminhar(self):
+        print("Caminhei, foda")
+        opcoes = list()
+        for i in range(nGrafo):
+            if data[self.caminho[-1][0]][i]:
+                opcoes.append(i)
+                #isso aq tá fazendo com que toda a vez q a formiga caminhe ela sempre vai pegar as possibilidades com base no último lugar q ela tá
+                # dessa maneira toda vez q ela caminha ela faz a entrada desse número em 2 pontos ao mesmo tempo:
+                # matriz caminho: [[0,0]] <-inicial
+                # matriz caminho: [[0,x], [x]] <-escolheu o caminho x a partir do inicial
+                # matriz caminho: [[0,x], [x, y], [y]] <-escolheu o caminho y a partir do x
+                #....
 
-# caminho = [
-#  """ linha """ [3], [4]
-# ]
 
+popInicial = list()
+for i in range(nAnts):
+    caminho = [[0, 0]]
+    distTotal = 0
+    formiga = Formiga(caminho, distTotal)
+    popInicial.append(formiga)
+
+for formiga in popInicial:
+    formiga.caminha()
 # formiga1 = Formiga(null, null)
 # formigas.append(formiga1)
 # matriz caminho = list()
