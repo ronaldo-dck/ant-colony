1d051f01d5b9a4e37ef0147500662b649e650fe7
Ronaldo Drecksler <ronaldodrecksler@pop-os.localdomain>
2023-01-21 02:36:13 -0300

    Matriz de Rotas

--- antColony.py
+++ antColony.py
@@ -1,10 +1,41 @@
 import numpy as np
 import pandas as pd
 
-nGrafo = '10' #input("Qual grafo deseja carregar:")
-path = './entradasGrafos/Entrada ' + nGrafo + '.txt'
+nGrafo = 10 #input("Qual grafo deseja carregar:")
+path = f'./entradasGrafos/Entrada {nGrafo}.txt'
 #data = pd.read_csv(path, index_col=None, header=0, sep=' ', false_values=np.NaN)
 data = pd.read_csv(path, index_col=None, header=None)
+
+
+# teste = np.zeros((nGrafo,nGrafo))
+# print(teste[1])
+
+# teste[1][2] = 7
+
+# print(teste[1])
+
+
+class Rotas:
+    def __init__(self, distancia, umSobreD, feromonio):
+        self.distancia = distancia
+        self.umSobreD = umSobreD
+        self.feromonio = feromonio
+
+teste = list()
+for i in range(nGrafo):
+    aux = list()
+    teste.append(aux)
+    for j in range(nGrafo):
+        teste[i].append(0)
+        
+for i in range(nGrafo):
+    for j in range(nGrafo):
+        rota = Rotas(data[i][j], 1/data[i][j] if data[i][j] else 0, 1)
+        teste[i][j] = rota
+        
+print(teste[0][3].distancia)
+print(teste[0][4].distancia)
+
 
 class Formiga:
     def __init__(self, caminho, distancia):
@@ -18,24 +49,25 @@ class Formiga:
 #  """ linha """ [3], [4]
 # ]
 
-
+# formiga1 = Formiga(null, null)
+# formigas.append(formiga1)
 # matriz caminho = list()
 # inteiro distancia = 0
 # for linha
 #    int possibilidades = 0
+#    opcoes = list()
 #    for coluna
 #      se data[linha][coluna] != 0
 #         possibilidades++
+#         opcoes.append([linha, coluna])
 #    chance = 1/possibilidades * 100
-#    for coluna
-#      # possibilidades caminhos : chance% cada
+#    # possibilidades caminhos : chance% cada
+#    for i in possibilidades
 #      aleatorio = 0 a 99
 #      se aleatorio < chance
-#        caminho.append([linha, coluna])
-#        distancia += data[linha][coluna]
-# formiga1 = Formiga(caminho, distancia)
-# formigas.append(formiga1)
-
+#         caminho.append(opcoes[i])
+#         distancia += data[opcoes[i][0]][opcoes[i][1]]
+#         break
 
 # formiga.caminhar()
 
